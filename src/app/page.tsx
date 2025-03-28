6f0c0842c16d35a21034425776057f8f44c37892
kbnopro <tuankhanhb00@gmail.com>
2025-03-28 19:25:11 +1100

    feat: login page, remove redundant table and database script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,22 @@
-import Link from "next/link";
-
 import { auth } from "@/server/auth";
-import { HydrateClient } from "@/trpc/server";
+import { redirect } from "next/navigation";
+import { signOut } from "@/server/auth";
 
 export default async function Home() {
   const session = await auth();
+  if (!session) redirect("/login");
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create
-            {" "}
-            <span className="text-[hsl(280,100%,70%)]">T3</span>
-            {" "}
-            App
-          </h1>
-          <div className="flex flex-col items-center gap-2">
-            <div className="flex flex-col items-center justify-center gap-4">
-              {session && (
-                <span>
-                  Logged in as
-                  {" "}
-                  {session.user?.name}
-                </span>
-              )}
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
-          </div>
-        </div>
-      </main>
-    </HydrateClient>
+    <main>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
+      This is home page
+    </main>
   );
 }
