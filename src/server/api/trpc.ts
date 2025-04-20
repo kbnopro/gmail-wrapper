102399719346260b89dacd5b0a0aa812c35c3c7b
kbnopro <tuankhanhb00@gmail.com>
2025-04-20 21:04:03 +1000

    feat: remove db from trpc context

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -12,7 +12,6 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { auth } from "@/server/auth";
-import { db } from "@/server/db";
 
 /**
  * 1. CONTEXT
@@ -30,7 +29,6 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
   const session = await auth();
 
   return {
-    db,
     session,
     ...opts,
   };
@@ -90,7 +88,7 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
   if (t._config.isDev) {
     // artificial delay in dev
     const waitMs = Math.floor(Math.random() * 400) + 100;
-    await new Promise(resolve => setTimeout(resolve, waitMs));
+    await new Promise((resolve) => setTimeout(resolve, waitMs));
   }
 
   const result = await next();
